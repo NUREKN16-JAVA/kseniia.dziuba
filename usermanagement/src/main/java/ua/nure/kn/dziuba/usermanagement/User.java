fec4e4165bad6dea314f106ff37e58a64e541af1
kseniia-dziuba-NURE <kseniia.dziuba@nure.ua>
2018-11-10 20:29:54 +0200

    Initial commit for usermanagment

--- usermanagement/src/main/java/ua/nure/kn/dziuba/usermanagement/User.java
+++ usermanagement/src/main/java/ua/nure/kn/dziuba/usermanagement/User.java
@@ -1,31 +1,24 @@
 package ua.nure.kn.dziuba.usermanagement;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
-import static java.time.temporal.ChronoUnit.DAYS;
+public class User implements Serializable {
 
-public class User implements java.io.Serializable {
+    private static final long serialVersionUID = 5193357156492322710L;
     private Long id;
     private String firstName;
     private String lastName;
     private Date dateOfBirth;
 
-    private final int ID = 1;
-    private final String FIRST_NAME = "Kseniia";
-    private final String LAST_NAME = "Dziuba";
-    private final Date DATE_OF_BIRTH = new SimpleDateFormat("d-MMM-yyyy").parse("11-Aug-1999");
+    public User(){}
 
-
-    public User() throws ParseException {
-        setId((long) ID);
-        setFirstName(FIRST_NAME);
-        setLastName(LAST_NAME);
-        setDateOfBirth(DATE_OF_BIRTH);
+    public User(Long id, String firstName, String lastName, Date dateOfBirth) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
     }
 
     public Long getId() {
@@ -60,55 +53,41 @@ public class User implements java.io.Serializable {
         this.dateOfBirth = dateOfBirth;
     }
 
+    /**
+     * Returns User's full name.
+     * Format: "lastName, firstName"
+     * For example: "Dziuba, Kseniia"
+     *
+     * @return String concatenation of last and first names.
+     */
     public String getFullName() {
-        StringBuilder fullNameString = new StringBuilder();
-        fullNameString.append(getLastName()).append(", ").append(getFirstName());
-        return fullNameString.toString();
+        return (new StringBuilder(getLastName()))
+                .append(", ")
+                .append(getFirstName())
+                .toString();
     }
 
-    public long getAge() throws ParseException {
+    /**
+     * Returns User's age based on User's birth date and current system date.
+     *
+     * @return int User's age.
+     */
+    public int getAge() {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        int currentYear = calendar.get(Calendar.YEAR);
-        User user = new User();
-        calendar.setTime(user.getDateOfBirth());
-        int userYearOfBirth = calendar.get(Calendar.YEAR);
-        return currentYear - userYearOfBirth;
-    }
-
-    public long getAge(int userYearOfBirth) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        int currentYear = calendar.get(Calendar.YEAR);
-        return currentYear - userYearOfBirth;
-    }
-
-    public long getAge(int userYearOfBirth, String userMonthOfBirth) throws ParseException {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
         int currentYear = calendar.get(Calendar.YEAR);
         int currentMonth = calendar.get(Calendar.MONTH);
-        Date monthDate = new SimpleDateFormat("MMM", Locale.ENGLISH).parse(userMonthOfBirth);
-        calendar.setTime(monthDate);
-        int month = calendar.get(Calendar.MONTH);
-        return ((currentYear * 12) + currentMonth) - ((userYearOfBirth * 12) + month);
-    }
+        int currentDayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
 
-    public long getAge(int userYearOfBirth, int userMonthOfBirth) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        int currentYear = calendar.get(Calendar.YEAR);
-        int currentMonth = calendar.get(Calendar.MONTH);
-        return ((currentYear * 12) + currentMonth) - ((userYearOfBirth * 12) + userMonthOfBirth);
-    }
+        calendar.setTime(getDateOfBirth());
+        int yearOfBirth = calendar.get(Calendar.YEAR);
+        int monthOfBirth = calendar.get(Calendar.MONTH);
+        int dayOfBirth = calendar.get(Calendar.DAY_OF_MONTH);
 
-    public long getAge(int userYearOfBirth, int userMonthOfBirth, int userDayOfBirth) {
-        LocalDate dateOfBirth = LocalDate.of(userYearOfBirth, userMonthOfBirth, userDayOfBirth);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        LocalDate curDate = LocalDate.of(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-        long daysBetween = DAYS.between(dateOfBirth, curDate);
+        int currentAge = currentYear - yearOfBirth;
+        if (monthOfBirth >= currentMonth && dayOfBirth >= currentDayOfMonth) {
+            return currentAge - 1;
+        }
 
-        return daysBetween;
+        return currentAge;
     }
 }
--- usermanagement/src/test/java/ua/nure/kn/dziuba/usermanagement/UserTest.java
+++ usermanagement/src/test/java/ua/nure/kn/dziuba/usermanagement/UserTest.java
@@ -3,60 +3,179 @@ package ua.nure.kn.dziuba.usermanagement;
 import junit.framework.TestCase;
 
 import java.text.ParseException;
-import java.time.LocalDate;
 import java.util.Calendar;
-import java.util.Date;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 public class UserTest extends TestCase {
 
     private User user;
+
+    private final String FULL_NAME_ETALONE = "Dziuba, Kseniia";
+    private final String FIRST_NAME_ETALONE = "Kseniia";
+    private final String LAST_NAME_ETALONE = "Dziuba";
+
+    private static final int YEAR_OF_BIRTH = 1999;
+
+    //Birthday has been passed in this year, month = month_of_birth
+    private static final int ETALONE_AGE_1 = 19;
+    private static final int MONTH_OF_BIRTH_1 = 8;
+    private static final int DAY_OF_BIRTH_1 = 11;
+
+    //Birthday hasn't been passed in this year, current month < month_of_birth
+    private static final int ETALONE_AGE_2 = 18;
+    private static final int MONTH_OF_BIRTH_2 = 12;
+    private static final int DAY_OF_BIRTH_2 = 11;
+
+    //Birthday has been passed in this year, in this month
+    private static final int ETALONE_AGE_3 = 19;
+    private static final int MONTH_OF_BIRTH_3 = 11;
+    private static final int DAY_OF_BIRTH_3 = 9;
+
+    //Birthday hasn't been passed in this year, will pass in this month
+    private static final int ETALONE_AGE_4 = 18;
+    private static final int MONTH_OF_BIRTH_4 = 11;
+    private static final int DAY_OF_BIRTH_4 = 11;
+
+    //Birthday was yesterday
+    private static final int ETALONE_AGE_5 = 19;
+    private static final int MONTH_OF_BIRTH_5 = 11;
+    private static final int DAY_OF_BIRTH_5 = 9;
+
+    //Birthday will be tomorrow
+    private static final int ETALONE_AGE_6 = 18;
+    private static final int MONTH_OF_BIRTH_6 = 11;
+    private static final int DAY_OF_BIRTH_6 = 11;
+
+    //Birthday is the 31 of December
+    private static final int ETALONE_AGE_7 = 18;
+    private static final int MONTH_OF_BIRTH_7 = 12;
+    private static final int DAY_OF_BIRTH_7 = 31;
+
+    //Birthday is the 1 of January
+    private static final int ETALONE_AGE_8 = 19;
+    private static final int MONTH_OF_BIRTH_8 = 1;
+    private static final int DAY_OF_BIRTH_8 = 1;
+
+    public UserTest() throws ParseException {
+    }
 
     public void setUp() throws Exception {
         user = new User();
         super.setUp();
     }
 
-    public void testGetFullName(){
-        assertEquals("Dziuba, Kseniia", user.getFullName());
+    /**
+     * Tests User's full name.
+     * <p>
+     * Expected result: LAST_NAME_ETALONE, FIRST_NAME_ETALONE
+     */
+    public void testGetFullName() {
+        user.setFirstName(FIRST_NAME_ETALONE);
+        user.setLastName(LAST_NAME_ETALONE);
+
+        assertEquals(FULL_NAME_ETALONE, user.getFullName());
     }
 
-    public void testGetAgeYearsWithoutParameters() throws ParseException {
+    /**
+     * Tests User's age when Birthday passed in this year.
+     * <p>
+     * expected result: ETALONE_AGE_1
+     */
+    public void testGetAge_BirthDayPassedInThisYear_ReturnsAge() {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(user.getDateOfBirth());
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_1, DAY_OF_BIRTH_1);
 
-        assertEquals(2018 - 1999, user.getAge());
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_1, user.getAge());
     }
 
-    public void testGetAgeYears(){
+    /**
+     * Tests User's age when Birthday will be in this year.
+     * <p>
+     * Expected result: ETALONE_AGE_2
+     */
+    public void testGetAge_BirthDayWillBeInThisYear_ReturnsAgeMinus1() {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(user.getDateOfBirth());
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_2, DAY_OF_BIRTH_2);
 
-        assertEquals(2018 - 1999, user.getAge(calendar.get(Calendar.YEAR)));
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_2, user.getAge());
     }
 
-    public void testGetAgeYearsMonthsString() throws ParseException {
+    /**
+     * Tests User's age when Birthday was in this month.
+     * <p>
+     * Expected result: ETALONE_AGE_3
+     */
+    public void testGetAge3_BirthDayWasInThisMonth_ReturnsAge() {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(user.getDateOfBirth());
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_3, DAY_OF_BIRTH_3);
 
-        assertEquals(((2018 * 12) + 10) - ((1999 * 12) + 8), user.getAge(calendar.get(Calendar.YEAR), "Aug"));
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_3, user.getAge());
     }
 
-    public void testGetAgeYearsMonths(){
+    /**
+     * Tests User's age when Birthday will be in this month.
+     * <p>
+     * Expected result: ETALONE_AGE_4
+     */
+    public void testGetAge_BirthDayWillBeInThisMonth_ReturnsAgeMinus1() {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(user.getDateOfBirth());
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_4, DAY_OF_BIRTH_4);
 
-        assertEquals(((2018 * 12) + 10) - ((1999 * 12) + 8), user.getAge(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)));
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_4, user.getAge());
     }
 
-    public void testGetAgeYearsMonthsDays() {
+    /**
+     * Tests User's age when Birthday was yesterday.
+     * <p>
+     * Expected result: ETALONE_AGE_5
+     */
+    public void testGetAge_BirthDayWasYesterday_ReturnsAge() {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        LocalDate curDate = LocalDate.of(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-        calendar.setTime(user.getDateOfBirth());
-        LocalDate BirthDate = LocalDate.of(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_5, DAY_OF_BIRTH_5);
 
-        assertEquals(DAYS.between(BirthDate, curDate), user.getAge(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)));
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_5, user.getAge());
+    }
+
+    /**
+     * Tests User's age when Birthday will be tomorrow.
+     * <p>
+     * Expected result: ETALONE_AGE_6
+     */
+    public void testGetAge_BirthDayWillBeTomorrow_ReturnsAgeMinus1() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_6, DAY_OF_BIRTH_6);
+
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_6, user.getAge());
+    }
+
+    /**
+     * Tests User's age when Birthday is 31 of December.
+     * <p>
+     * Expected result: ETALONE_AGE_7
+     */
+    public void testGetAge_BirthDayIs31OfDecember_ReturnsAgeMinus1() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_7, DAY_OF_BIRTH_7);
+
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_7, user.getAge());
+    }
+
+    /**
+     * Tests User's age when Birthday is the 1 of January.
+     * <p>
+     * Expected result: ETALONE_AGE_8
+     */
+    public void testGetAge_BirthDayIs1OfJanuary_ReturnsAge() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR_OF_BIRTH, MONTH_OF_BIRTH_8, DAY_OF_BIRTH_8);
+
+        user.setDateOfBirth(calendar.getTime());
+        assertEquals(ETALONE_AGE_8, user.getAge());
     }
 }
