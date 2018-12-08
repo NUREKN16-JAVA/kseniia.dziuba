ddb4c2bbf2ec3b5cbff8bde62b20bcdc9856a656
kseniia-dziuba-NURE <kseniia.dziuba@nure.ua>
2018-12-08 22:01:08 +0200

    add browsepanel class

---
+++ usermanagement/src/main/java/ua/nure/kn/dziuba/usermanagement/gui/BrowsePanel.java
@@ -0,0 +1,9 @@
+package ua.nure.kn.dziuba.usermanagement.gui;
+
+import javax.swing.*;
+
+public class BrowsePanel extends JPanel {
+    public BrowsePanel(MainFrame frame){
+
+    }
+}
--- usermanagement/src/main/java/ua/nure/kn/dziuba/usermanagement/gui/MainFrame.java
+++ usermanagement/src/main/java/ua/nure/kn/dziuba/usermanagement/gui/MainFrame.java
@@ -1,10 +1,46 @@
 package ua.nure.kn.dziuba.usermanagement.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
 
-    public static void main(String args[]){
+    private JPanel contentPanel;
+    private BrowsePanel browsePanel;
+    private static final int FRAME_WIDTH = 600;
+    private static final int FRAME_HEIGHT = 800;
 
+    public MainFrame(){
+        super();
+        initialize();
+    }
+
+    private void initialize(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setTitle("User management");
+        this.setContentPane(getContentPanel());
+    }
+
+    private JPanel getContentPanel(){
+        if(contentPanel == null){
+            contentPanel = new JPanel();
+            contentPanel.setLayout(new BorderLayout());
+            contentPanel.add(getBrowsePanel(), BorderLayout.CENTER);
+        }
+
+        return contentPanel;
+    }
+
+    private BrowsePanel getBrowsePanel(){
+        if(browsePanel == null){
+            browsePanel = new BrowsePanel(this);
+        }
+        return browsePanel;
+    }
+
+    public static void main(String args[]){
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
     }
 }
