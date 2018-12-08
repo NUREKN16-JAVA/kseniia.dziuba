6c0ec0bdbbfd2e5a86ee09c67411465a846e82a1
kseniia-dziuba-NURE <kseniia.dziuba@nure.ua>
2018-12-08 23:03:50 +0200

    add methods to AddPanel class

--- usermanagement/src/main/java/ua/nure/kn/dziuba/usermanagement/gui/AddPanel.java
+++ usermanagement/src/main/java/ua/nure/kn/dziuba/usermanagement/gui/AddPanel.java
@@ -1,6 +1,69 @@
 package ua.nure.kn.dziuba.usermanagement.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AddPanel extends JPanel {
+public class AddPanel extends JPanel implements ActionListener {
+
+    private MainFrame parent;
+    private JPanel buttonPanel;
+    private JPanel fieldPanel;
+    private JButton okButton;
+    private JButton cancelButton;
+
+    public AddPanel(MainFrame frame){
+        parent = frame;
+        initialize();
+    }
+
+    private void initialize() {
+        this.setLayout(new BorderLayout());
+        this.add(getFieldPanel(), BorderLayout.NORTH);
+        this.add(getButtonsPanel(), BorderLayout.SOUTH);
+    }
+
+    private Component getButtonsPanel() {
+        if(buttonPanel == null){
+            buttonPanel = new JPanel();
+            buttonPanel.add(getOkButton(), null);
+            buttonPanel.add(getCancelButton(), null);
+        }
+        return buttonPanel;
+    }
+
+    private JButton getCancelButton() {
+        if(cancelButton == null){
+            cancelButton = new JButton();
+            cancelButton.setText("Cancel");
+            cancelButton.setName("cancelButtton");
+            cancelButton.setActionCommand("cancel");
+            cancelButton.addActionListener(this);
+        }
+        return cancelButton;
+    }
+
+    private JButton getOkButton() {
+        if(okButton == null){
+            okButton = new JButton();
+            okButton.setText("OK");
+            okButton.setName("okButton");
+            okButton.setActionCommand("ok");
+            okButton.addActionListener(this);
+        }
+        return okButton;
+    }
+
+    private Component getFieldPanel() {
+        if(fieldPanel == null){
+            fieldPanel = new JPanel();
+        }
+        return fieldPanel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
