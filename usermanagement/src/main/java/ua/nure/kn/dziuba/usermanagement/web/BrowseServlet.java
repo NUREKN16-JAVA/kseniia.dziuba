f0fe263c86ac036966837cd87c3c24da5b26fc93
kseniia-dziuba-NURE <kseniia.dziuba@nure.ua>
2018-12-20 23:04:49 +0200

    add browse() method to BrowseServlet class

--- usermanagement/src/main/java/ua/nure/kn/dziuba/usermanagement/web/BrowseServlet.java
+++ usermanagement/src/main/java/ua/nure/kn/dziuba/usermanagement/web/BrowseServlet.java
@@ -1,6 +1,33 @@
 package ua.nure.kn.dziuba.usermanagement.web;
 
+import ua.nure.kn.dziuba.usermanagement.db.DaoFactory;
+import ua.nure.kn.dziuba.usermanagement.db.DatabaseException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+import static junit.framework.Assert.fail;
 
 public class BrowseServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        browse(req, resp);
+    }
+
+    private void browse(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        Collection users;
+        try {
+            users = DaoFactory.getInstance().getUserDao().findAll();
+            req.getSession().setAttribute("users", users);
+            req.getRequestDispatcher("/browse.jsp").forward(req, resp);
+        } catch (DatabaseException e) {
+            throw new ServletException(e);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
 }
