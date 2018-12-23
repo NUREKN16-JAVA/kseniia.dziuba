b33e4c49f1926780ad5b0e5f298a8fd82ea3fa9b
kseniia-dziuba-NURE <kseniia.dziuba@nure.ua>
2018-12-23 23:15:11 +0200

    add BrowseServlet tests

--- usermanagement/src/test/java/ua/nure/kn/dziuba/usermanagement/web/BrowseServletTest.java
+++ usermanagement/src/test/java/ua/nure/kn/dziuba/usermanagement/web/BrowseServletTest.java
@@ -2,6 +2,7 @@ package ua.nure.kn.dziuba.usermanagement.web;
 
 import junit.framework.TestCase;
 import ua.nure.kn.dziuba.usermanagement.User;
+import ua.nure.kn.dziuba.usermanagement.db.DatabaseException;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -41,5 +42,36 @@ public class BrowseServletTest extends MockServletTestCase {
         User sessionUser = (User) getWebMockObjectFactory().getMockSession().getAttribute("user");
         assertNotNull("Couldn't find user", sessionUser);
         assertSame("Wrong user was found", user, sessionUser);
+    }
+
+    public void testDetails() {
+        User expectedUser = createUserForTest();
+        getMockUserDao().expectAndReturn("find", new Long(1000), expectedUser);
+        addRequestParameter("detailsButton", "Details");
+        addRequestParameter("id", "1000");
+        doPost();
+        User actualUser = (User) getWebMockObjectFactory().getMockSession().getAttribute("user");
+        assertNotNull("Could not find user in session", actualUser);
+        assertEquals(expectedUser, actualUser);
+    }
+
+    public void testDelete() {
+        User expectedUser = createUserForTest();
+        getMockUserDao().expectAndReturn("find", new Long(1000), expectedUser);
+        getMockUserDao().expect("delete", expectedUser);
+        addRequestParameter("deleteButton", "Delete");
+        addRequestParameter("id", "1000");
+        doPost();
+    }
+
+    public void testDeleteWithInvalidId() {
+        String expectedErrorMessage = "Incorrect id";
+        getMockUserDao().expectAndThrow("find", new Long(1000), new DatabaseException(expectedErrorMessage));
+        addRequestParameter("deleteButton", "Delete");
+        addRequestParameter("id", "1000");
+        doPost();
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute("error");
+        assertNotNull("Could not find error message", errorMessage);
+        assertTrue(errorMessage.contains(errorMessage));
     }
 }
